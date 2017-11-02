dbcc0c6991baae2af12095dfdf6eb8ad722c985c
SwinJoy <1066684692@qq.com>
2017-11-02 17:30:30 +0800

    通讯卫士

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/db/BlackNumberOpenHelper.java
@@ -0,0 +1,33 @@
+package cn.edu.gdmec.android.mobileguard.m3communicationguard.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Swindler on 2017/10/30.
+ */
+
+public class BlackNumberOpenHelper extends SQLiteOpenHelper{
+    private static  String DB_NAME = "my_info";
+    private static  int VERSION = 1;
+    public BlackNumberOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,factory,version);
+        this.DB_NAME = name;
+        this.VERSION = version;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table blacknumber"+
+                "(id integer primary key autocrement,"+
+                "number varchar(20),"+
+                "name varchar(255),"+
+                "mode interger)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
