bad08c1a0fe544d6b35c8ce0bafe0ed124f511f3
balrdsky <514309517@qq.com>
2017-11-22 22:40:09 +0800

    更新缓存清理

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,8 +1,8 @@
 package cn.edu.gdmec.android.mobileguard;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import cn.edu.gdmec.android.mobileguard.m1hone.HomeActivity;
@@ -16,23 +16,29 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         mVersion = MyUtils.getVersion(getApplicationContext());
         mTvVersion = (TextView)findViewById(R.id.tv_splash_version);
         mTvVersion.setText("版本号："+mVersion);
 
-        /*final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+        final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback, HomeActivity.class);
           new Thread(){
             @Override
             public void run(){
                 super.run();
-                versionUpdateUtils.getCloudVersion();
+                //versionUpdateUtils.getCloudVersion();
                 versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
             }
-        }.start()*/
+        }.start();
 
 
         startActivity(new Intent(this, HomeActivity.class));
         finish();
     }
+    VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
+        @Override
+        public void afterDownload(String filename) {
+
+        }
+    };
 }
